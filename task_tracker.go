b24e502f0da0a4d81a472ed4aeab26af863edafa
Samuel Orchestrator Williams <samuelowilliams.dev@gmail.com>
2025-08-14 22:51:56 +0200

    feat: Implemented checkCommand function

--- task_tracker.go
+++ task_tracker.go
@@ -1,7 +1,76 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-func main () {
-	fmt.Println("Hello Orchestrator")
+func checkCommand(arguments []string, argumentsLength int) {
+	var command string = arguments[0]
+	switch command {
+	case "add":
+		if argumentsLength == 2 {
+			fmt.Println("Run add command")
+		} else {
+			fmt.Println("No argument was found: Kindly Input argument for add command")
+		}
+	case "delete":
+		if argumentsLength == 2 {
+			fmt.Println("Run delete command")
+		} else {
+			fmt.Println("No argument was found: Kindly Input argument for delete command")
+		}
+	case "update":
+		if argumentsLength == 3 {
+			fmt.Println("Run update command")
+		} else {
+			fmt.Println("No argument was found: Kindly Input argument for update command")
+		}
+	case "mark-in-progress":
+		if argumentsLength == 2 {
+			fmt.Println("Run mark-in-progress command")
+		} else {
+			fmt.Println("No argument was found: Kindly Input argument for mark-in-progress command")
+		}
+	case "mark-done":
+		if argumentsLength == 2 {
+			fmt.Println("Run mark-done command")
+		} else {
+			fmt.Println("No argument was found: Kindly Input argument for mark-done command")
+		}
+	case "list":
+
+		var subCommand string
+		if argumentsLength == 2 {
+			subCommand = arguments[1]
+		} else {
+			subCommand = ""
+		}
+		switch subCommand {
+		case "todo":
+
+			fmt.Println("Run list todo command")
+
+		case "in-progress":
+
+			fmt.Println("Run list in-progress command")
+		case "done":
+
+			fmt.Println("Run list done command")
+		default:
+			fmt.Println("Run list command")
+		}
+
+	}
+}
+
+func main() {
+	arguments := os.Args[1:]
+	argumentsLength := len(arguments)
+	if argumentsLength == 0 {
+		fmt.Println("No command was entered: Kindly input a  command")
+		return
+	}
+	checkCommand(arguments, argumentsLength)
+	fmt.Println("Length of arguments", argumentsLength)
 }
